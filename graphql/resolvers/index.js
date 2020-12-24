f3974c073d24c3293e53c0e9fb7584e0f78b13f4
will kerwin <will@itskerwin.co.uk>
2020-12-24 20:52:49 +0000

    added framework for registration

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,8 +1,12 @@
 const postsResolvers = require("./posts");
+const { Mutation } = require("./users");
 const usersResolvers = require("./users");
 
 module.exports = {
     Query: {
         ...postsResolvers.Query
+    },
+    Mutation:{
+        ...usersResolvers.Mutation
     }
 }
